8b68ef8ce931641219b8a8d1b0ebcb6afcf91883
Chex Possible <chexers.775@gmail.com>
2017-08-08 06:32:40 +0000

    various

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,23 @@
+# === Abbreviations
+#       generic
+abbr -a gs git   status
+abbr -a ai apt   install -y 
+abbr -a as apt   search 
 
-abbr -a gs git  status
-abbr -a ai sudo apt install -y 
-abbr -a as apt  search 
+#       service management
+abbr -a start    sudo systemctl start
+abbr -a status   sudo systemctl status
+abbr -a restart  sudo systemctl restart
+abbr -a stop     sudo systemctl stop
 
-abbr -a start   sudo systemctl start
-abbr -a status  sudo systemctl status
-abbr -a restart sudo systemctl restart
-abbr -a stop    sudo systemctl stop
+#       root-only
+abbr -a gem      sudo gem
+abbr -a svim     sudo vim
+abbr -a iwconfig sudo iwconfig 
+abbr -a ifconfig sudo ifconfig 
+
+set -x EDITOR vim
+set -x SHELL  (which fish)
 
 
 if status --is-interactive
