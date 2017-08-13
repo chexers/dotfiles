308977661759775c272ed927013d52325f9a6baf
Chex Possible <chexers.775@gmail.com>
2017-08-13 16:44:01 +0000

    sooprise

--- fish/config.fish
+++ fish/config.fish
@@ -16,10 +16,20 @@ abbr -a svim     sudo vim
 abbr -a iwconfig sudo iwconfig 
 abbr -a ifconfig sudo ifconfig 
 
-set -x EDITOR vim
-set -x SHELL  (which fish)
+set -xg EDITOR vim
+set -xg SHELL  (which fish)
 
+set -xg PATH $HOME/.local/bin $HOME/.gem/ruby/2.4.0/bin $PATH
 
 if status --is-interactive
     eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
 end
+
+function hybrid_bindings --description "Vi-style bindings that inherit"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings hybrid_bindings
+fish_vi_kdy_bindings insert
