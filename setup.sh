f54586bbe8915bebf8148e2b20495cdd0c2b4cc1
Joshua Kogut <chexers.775@gmail.com>
2021-02-26 13:02:31 -0800

    added setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+source ./_init/000_environment.sh
+source ./_init/020_structure.sh
+source ./_init/030_vim.sh
+source ./_init/100_base16shell.sh
+source ./_init/998_git-config.sh
+source ./_init/999_ssh-keygen.sh
+
+print "Done setting up your dotfiles!~"
